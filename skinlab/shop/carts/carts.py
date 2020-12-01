5689dc760bff9a65ed4d367c9a7984f9010e0171
joevidal5 <62678280+joevidal5@users.noreply.github.com>
2020-12-01 16:13:58 -0300

    nose

--- skinlab/shop/carts/carts.py
+++ skinlab/shop/carts/carts.py
@@ -1,6 +1,7 @@
 from flask import redirect, render_template, url_for, flash, request, session, current_app
 from shop import db, app
 from shop.products.models import Addskin
+from shop.products.models import Brand, Category, Addskin
 
 def MargerDicts(dict1, dict2):
     if isinstance(dict1, list) and isinstance (dict2, list):
@@ -16,7 +17,7 @@ def AddCart():
         quantity = request.form.get('quantity')
         skin = Addskin.query.filter_by(id=skin_id).first()
         if skin_id and quantity and request.method == 'POST':
-            DictItems = {skin_id:{'name':skin.name, 'price':skin.price, 'float':skin.float, 'quantify':quantity, 'image':skin.image}}
+            DictItems = {skin_id:{'name':skin.name, 'price':skin.price, 'float':skin.float, 'quantity':quantity, 'image':skin.image}}
 
             if 'Shoppingcart' in session:
                 print(session['Shoppingcart'])
@@ -32,3 +33,46 @@ def AddCart():
         print(e)
     finally:
         return redirect(request.referrer)
+
+
+@app.route('/carts')
+def getCart():
+    barnds = Brand.query.join(Addskin, (Brand.id == Addskin.brand_id)).all()
+    categories = Category.query.join(Addskin, (Category.id == Addskin.category_id)).all()
+    if 'Shoppingcart' not in session or len(session['Shoppingcart']) <= 0:
+        return redirect(url_for('home'))
+    grantotal = 0
+    for key, skin in session['Shoppingcart'].items():
+        grantotal += float(skin['price']*int(skin['quantity']))
+    return render_template('products/carts.html' , grantotal = grantotal,collections=barnds, categories=categories )
+
+@app.route('/updatecart/<int:code>', methods = ['POST'])
+def updatecart(code):
+    if 'Shoppingcart' not in session and len(session['Shoppingcart']) <= 0:
+        return redirect(url_for('home'))
+    if request.method == "POST":
+        quantity = request.form.get('quantity')
+        try:
+            session.modified = True
+            for key, item in session['Shoppingcart'].items():
+                if int(key) ==  code:
+                    item['quantity'] = quantity
+                    flash('Item actualizado')
+                    return redirect(url_for('getCart'))
+        except Exception as e:
+            print(e)
+            return redirect(url_for('getCart'))
+
+@app.route('/removecart/<int:id>')
+def deleteitem(id):
+    if 'Shoppingcart' not in session and len(session['Shoppingcart']) <= 0:
+        return redirect(url_for('home'))
+    try:
+        session.modified = True
+        for key , item in session['Shoppingcart'].items():
+            if int(key) == id:
+                session['Shoppingcart'].pop(key, None)
+                return redirect(url_for('getCart'))
+    except Exception as e:
+        print(e)
+        return redirect(url_for('getCart'))
